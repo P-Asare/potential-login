f7ec1369f9c44125af24f36789461932d51be005
P-Asare <pinopalal11@gmail.com>
2023-07-21 13:32:28 +0100

    add image and first texts

--- lib/src/features/authentication/screens/login_screen/login_screen.dart
+++ lib/src/features/authentication/screens/login_screen/login_screen.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
-
+import 'package:login_app/src/constants/image_strings.dart';
+import 'package:login_app/src/constants/text_strings.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-            ],
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image(
+                  image: const AssetImage(welcomeImage),
+                  height: screenHeight * 0.2,
+                ),
+                Text(
+                  mainTextLogin,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Text(
+                  subTextLogin,
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+
+                /*Second section of page */
+              ],
+            ),
           ),
         ),
       ),
