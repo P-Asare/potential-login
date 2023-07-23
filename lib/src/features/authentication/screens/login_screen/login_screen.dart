68c1df27b78eac849baa6df670d8a2847f7b2190
P-Asare <pinopalal11@gmail.com>
2023-07-24 00:34:14 +0100

    clean up code

--- lib/src/features/authentication/screens/login_screen/login_screen.dart
+++ lib/src/features/authentication/screens/login_screen/login_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:login_app/src/constants/image_strings.dart';
-import 'package:login_app/src/constants/text_strings.dart';
-
 import 'login_footer.dart';
 import 'login_form.dart';
 import 'login_header.dart';
@@ -22,12 +19,8 @@ class LoginScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 LoginHeader(screenHeight: screenHeight),
-
-                /*Second section of page */
                 const LoginForm(),
-
-                /*Third section */
-                const LoginFooter()
+                const LoginFooter(),
               ],
             ),
           ),
