4e6ac53fb94375d082a4c2f84492db1e7702e81d
P-Asare <pinopalal11@gmail.com>
2023-07-24 03:59:01 +0100

    add form

--- lib/src/features/authentication/screens/signup_screen/signup_screen.dart
+++ lib/src/features/authentication/screens/signup_screen/signup_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:login_app/src/constants/image_strings.dart';
 import 'package:login_app/src/constants/text_strings.dart';
 
+import 'signup_form.dart';
 import 'signup_header.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -25,70 +26,7 @@ class SignUpScreen extends StatelessWidget {
             child: Column(
               children: [
                 SignupHeader(screenHeight: screenHeight),
-                Form(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 10,
-                    ),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.person),
-                            border: OutlineInputBorder(),
-                            hintText: formName,
-                            labelText: formName,
-                          ),
-                        ),
-                        SizedBox(
-                          height: boxSpace,
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.email_outlined),
-                            border: OutlineInputBorder(),
-                            hintText: email,
-                            labelText: email,
-                          ),
-                        ),
-                        SizedBox(
-                          height: boxSpace,
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.numbers),
-                            border: OutlineInputBorder(),
-                            hintText: formNumber,
-                            labelText: formNumber,
-                          ),
-                        ),
-                        SizedBox(
-                          height: boxSpace,
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.fingerprint),
-                            border: OutlineInputBorder(),
-                            hintText: password,
-                            labelText: password,
-                          ),
-                        ),
-                        SizedBox(
-                          height: boxSpace,
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: const Text(
-                              signUpString,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                SignupForm(boxSpace: boxSpace),
               ],
             ),
           ),
